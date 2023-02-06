2801c1eb689f65415aee4e7a433fe03cc739b8c2
Peter1907 <peter.nady.1408@gmail.com>
2023-02-06 23:23:45 +0200

    Fix linter errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,4 @@
 class Food < ApplicationRecord
-  belogns_to: :user
-  has_many: :recipes, through: :recipes_foods
+  belogns_to :user
+  has_many :recipes, through: :recipes_foods
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
-  belogns_to: :user
-  has_many: :foods, through: :recipes_foods
+  belogns_to :user
+  has_many :foods, through: :recipes_foods
 end
