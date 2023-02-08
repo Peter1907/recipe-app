3bdb4f62d118746950b7249936b69ec87266bd99
Peter1907 <peter.nady.1408@gmail.com>
2023-02-08 04:29:27 +0200

    Add delete function to the recipes & modify styles

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,10 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def destroy
+    Recipe.destroy(params[:id])
+    flash[:notice] = 'Recipe deleted'
+    redirect_to recipes_path
+  end
 end
