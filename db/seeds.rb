6b400a1b78c571d8a5125833527eef21ab84b835
Peter1907 <peter.nady.1408@gmail.com>
2023-02-08 02:15:55 +0200

    Add basic views & Fix relations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Recipe.destroy_all
+Food.destroy_all
+
+Recipe.create(name: "Pasta", preparation_time: 10, cooking_time: 20, description: "Pasta with tomato sauce", public: true, user_id: 1)
+Recipe.create(name: "Pizza", preparation_time: 10, cooking_time: 20, description: "Pizza with tomato sauce", public: true, user_id: 1)
+Recipe.create(name: "Rice", preparation_time: 10, cooking_time: 20, description: "Rice with tomato sauce", public: true, user_id: 1)
+Recipe.create(name: "Burger", preparation_time: 10, cooking_time: 20, description: "Burger with tomato sauce", public: true, user_id: 1)
+Recipe.create(name: "Salad", preparation_time: 10, cooking_time: 20, description: "Salad with tomato sauce", public: true, user_id: 1)
+
+# Food.create(name: "Tomato", measurement_unit: "kg", price: 1.5, quantity: 10, user_id: 1)
+# Food.create(name: "Onion", measurement_unit: "kg", price: 1.5, quantity: 10, user_id: 1)
+# Food.create(name: "Potato", measurement_unit: "kg", price: 1.5, quantity: 10, user_id: 1)
+# Food.create(name: "Carrot", measurement_unit: "kg", price: 1.5, quantity: 10, user_id: 1)
+# Food.create(name: "Cucumber", measurement_unit: "kg", price: 1.5, quantity: 10, user_id: 1)
+
+# Dish.create(food_id: 1, recipe_id: 1, quantity: 1)
+# Dish.create(food_id: 2, recipe_id: 1, quantity: 1)
+# Dish.create(food_id: 3, recipe_id: 1, quantity: 1)
+# Dish.create(food_id: 4, recipe_id: 1, quantity: 1)
+# Dish.create(food_id: 5, recipe_id: 1, quantity: 1)
