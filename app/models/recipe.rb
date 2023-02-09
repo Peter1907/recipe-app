ec578b9cf2de3bfb344387951ffd3bcb99a8a5f3
Peter1907 <peter.nady.1408@gmail.com>
2023-02-09 12:03:00 +0200

    Fix bug to separate each recipe's ingredient

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe < ApplicationRecord
   has_and_belongs_to_many :foods, join_table: :recipes_foods
 
   validates :name, presence: true, length: { maximum: 30 }
-  validates :description, presence: true, length: { maximum: 1000 }
+  validates :description, presence: true, length: { minimum: 20 }
   validates :cooking_time, presence: true, numericality: { greater_than: 0 }
   validates :preparation_time, presence: true, numericality: { greater_than: 0 }
   validates :public, inclusion: { in: [true, false] }
